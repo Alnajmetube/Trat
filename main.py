3c066f5e6685dfc397aa7eff33e6534088372172
muaid773 <muaidali773@gmail.com>
2026-09-06 11:25:20 +0300

    Build Termux AMD64

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import asyncio
 import html
+import json
 import os
+import sys
 # telegarm bot impoets
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 from telegram.error import NetworkError
 
-CHAT_ID = "__CHAT_ID__"
-TOKEN = "__TOKEN__"
+CONFIG_FILE = ".config.json"
+TOKEN = None
+CHAT_ID = None
 
+def install(token: str, chat_id: str):
+    config = {
+        "token": token,
+        "chat_id": str(chat_id)
+    }
+
+    with open(CONFIG_FILE, "w", encoding="utf-8") as f:
+        json.dump(config, f, ensure_ascii=False, indent=4)
+
+    print("✅ installation completed successfully.")
+
+def load_config():
+    global TOKEN, CHAT_ID
+
+    if not os.path.isfile(CONFIG_FILE):
+        print("❌ البرنامج غير مثبت.")
+        print('استخدم: script.py --install --token "TOKEN" --chat_id "CHAT_ID"')
+        sys.exit(1)
+
+    with open(CONFIG_FILE, "r", encoding="utf-8") as f:
+        config = json.load(f)
+
+    TOKEN = config["token"]
+    CHAT_ID = str(config["chat_id"])
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.id != CHAT_ID:
@@ -110,6 +137,22 @@ async def post_init(application):
     )
 
 def main():
+    if "--install" in sys.argv:
+        try:
+            token = sys.argv[sys.argv.index("--token") + 1]
+            chat_id = sys.argv[sys.argv.index("--chat_id") + 1]
+        except (ValueError, IndexError):
+            #in english
+            print(
+                '❌ Usage:\n'
+                'Trat--install --token "TOKEN" --chat_id "'
+            )
+            return
+
+        install(token, chat_id)
+        return
+
+    load_config()
     application = (
         ApplicationBuilder()
         .token(TOKEN)
